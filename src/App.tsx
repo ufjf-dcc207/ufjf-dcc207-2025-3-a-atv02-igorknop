7e50ba32039ba59bdae1661b24e2877ecd55318f
Igor Knop <igor.knop@ufjf.br>
2025-09-25 21:14:20 -0300

    :sparkles: adiciona componente Disciplina

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
+import Disciplina from './Disciplina';
 function App() {
   return (
     <>
       <h1>Olá mundo!</h1>
       <h2>Olá react!</h2>
+      <Disciplina/>
     </>
   );
 }
---
+++ src/Disciplina.tsx
@@ -0,0 +1,8 @@
+export default function Disciplina() {
+    return (
+        <>
+            <h1>DCC207</h1>
+            <h2>Desenvolvimento Web Front End</h2>
+        </>
+    )
+}
